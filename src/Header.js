f2f470dbdc62d476fbea5eff4cce920d8e4de756
likejean <popachs@yahoo.com>
2020-03-21 13:36:41 -0700

    Initial Setup

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import Header from "./Header";
 import './App.css';
 
-function App() {
+function Header() {
     return (
         <div className="App">
-            <Header/>
 
         </div>
     );
 }
 
-export default App;
+export default Header;
